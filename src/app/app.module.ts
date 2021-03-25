4f6186c3e7cb4db99651cde2fd768115a2dd8c1f
luankenzley <luankenzley@hotmail.com>
2021-03-25 18:40:59 -0300

    leitura de todos os livros por categoria implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
+import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 
 
 
@@ -44,6 +45,7 @@ import { CategoriaUpdateComponent } from './components/views/categoria/categoria
     CategoriaCreateComponent,
     CategoriaDeleteComponent,
     CategoriaUpdateComponent,
+    LivroReadAllComponent,
    
   ],
   imports: [
